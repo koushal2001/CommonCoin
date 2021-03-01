2fa3324090c52ff508d639691f13f41edb9ce627
Koushal Bhat <bhatkoushal2001@gmail.com>
2021-03-01 18:26:44 +0530

    LAtest commit

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask,render_template,flash,redirect,url_for,session,request,logging
 from passlib.hash import sha256_crypt
-from flaskext.mysql import MySQL
-
+from flask_mysqldb import MySQL
+from database import *
+from forms import *
 app =Flask(__name__)
 
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD'] = 'Tacmp@1234'
+app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'commonchain'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
+mysql.init_app(app)
 
+@app.route("/register" , methods={'GET','POST'})
+def register():
+    form=Register(request.form)
+    users=table("users","Name","Email","username","password")
+
+    if request.method == 'POST' and form.validate():
+        pass
+
+    return render_template('register.html')
 @app.route("/")
-@app.route("/index")
 def index():
+
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,16 @@
+from wtforms import Form, StringField, DecimalField, IntegerField, TextAreaField, PasswordField, validators
+
+#form used on Register page
+class Register(Form):
+    name = StringField('Full Name', [validators.Length(min=1,max=50)])
+    username = StringField('Username', [validators.Length(min=4,max=25)])
+    email = StringField('Email', [validators.Length(min=6,max=50)])
+    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match')])
+    confirm = PasswordField('Confirm Password')
+
+# class SendMoneyForm(Form):
+#     username = StringField('Username', [validators.Length(min=4,max=25)])
+#     amount = StringField('Amount', [validators.Length(min=1,max=50)])
+#
+# class BuyForm(Form):
+#     amount = StringField('Amount', [validators.Length(min=1,max=50)])
